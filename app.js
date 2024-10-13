01e58685070f160b3c64aa8aa48277f0792d794b
Rakesh Mahesh <rak73337@gmail.com>
2024-10-13 19:32:15 +0400

    added host privileges

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ joinGameButton.addEventListener('click', () => {
     addPlayer(playerName);
 
     // Check if player is the host
-    if (playerName.toLowerCase() === "rakeshfj3".toLowerCase()) {
+    if (playerName.toLowerCase() === 'rakesh'.toLowerCase()) {
       isHost = true;
       hostControlsDiv.style.display = 'block';
     }
